2eb0cbbf75501693067e21e63b7ad274b51b7a82
Naowak <yannisbendi@hotmail.Fr>
2015-10-29 12:33:23 +0100

    avance en cours, erreur incluse

--- sudoku.lisp
+++ sudoku.lisp
@@ -1,39 +1,87 @@
+(defconstant +SIZE+ 3)
+
 (defparameter +tabHash+
   (make-hash-table :test #'eq))
-
-(defparameter +grid+
-  (creer-grid 3))
 
 (defun creer-grid (size)
   (make-array (list (* size size) (* size size))))
 
-(defun creer-hash(size)
+(defparameter +grid+
+  (creer-grid +SIZE+))
+
+(defparameter +grid-access+
+  (creer-grid +SIZE+))
+
+(defun initialiser-hash(size)
   (if (> size 26)
       NIL
-      (creer-hash-term 
+      (initialiser-hash-term 
        size 
        (subseq 
 	'(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) 
 	0 size))))
 
-(defun creer-hash-term(size list)
+(defun initialiser-hash-term(size list)
   (setf (gethash (car (last list)) +tabHash+) size)
   (if (= 1 size)
       T
-      (creer-hash-term (1- size) (butlast list))))
-      
+      (initialiser-hash-term (1- size) (butlast list))))
+
+(defun init-all(size)
+  (initialiser-hash size)
+  (if (not (= size +SIZE+))
+      (progn 
+	(setf +grid+ (creer-grid size))
+	(setf +grid-access+ (creer-grid size)))
+      T))
       
 
-(defun set-tile(grid hauteur largeur valeur)
-  (let ((taille (car (array-dimensions grid)))
+(defun set-tile(hauteur largeur valeur)
+  (let ((taille (car (array-dimensions +grid+)))
 	(l (1- (getHash largeur +tabHash+)))
 	(h (1- hauteur)))
   (if (or (or (< valeur 1) (> valeur taille))
 	   (> h taille)
 	   (> l taille))
       NIL
-      (setf (aref grid h l) valeur)))) 
-      
+      (setf (aref +grid+ h l) valeur)))) 
+
+
+(defun test-ligne(n h valeur)
+  (if (< n 0)
+      T
+      (if (= (aref +grid+ h n) valeur)
+	  NIL
+	  (test-ligne (1- n) h valeur))))
+
+(defun test-colonne(n l valeur)
+  (if (< n 0)
+      T
+      (if (= (aref +grid+ n l) valeur)
+	  NIL
+	  (test-colonne l (1- n) valeur))))
+
+(defun test-carre-recX(n x y valeur)
+  (if (< n 0)
+      T
+      (if (test-carre-recY(1- +SIZE+) (+ n x) y valeur)
+	(test-carre-recX (1- n) x y valeur)
+	NIL)))
+
+(defun test-carre-recY(n x y valeur)
+  (if (< n 0)
+      T
+      (if (= (aref +grid+ (+ y n) x) valeur)
+	  NIL
+	  (test-carre-recY (1- n) x y valeur))))
+
+(defun test-carre(l h valeur)
+  (let ((x (floor (/ l +SIZE+)))
+	(y (floor (/ h +SIZE+))))
+    (test-carre-recX (1- +SIZE+) x y valeur)))
+    
+  
+  
       
       
   
